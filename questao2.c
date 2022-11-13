80469208d4e4b199dbee6924f903cae6a6a02944
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-13 16:57:20 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -1,53 +1,79 @@
 #include <stdio.h>
-#include <iostream>
-using namespace std;
-int main(){
-	
+
+int main()
+{
 	char op;
 	int suspeito, inocente;
 	
 	printf("Telefonou para a vitima? ");
 	scanf("%c", &op);
-	if (op=='s'){
-		suspeito++; 
-		if (op=='n'){
-			inocente++; } }
-			
+	if (op=='s')
+	{
+	suspeito++; 
+	if (op=='n')
+	{
+	inocente++; 
+	} 
+	}
+	fflush(stdin);
 	printf("Esteve no local do crime? ");
-	cin >> op;
-	if (op=='s'){
-		suspeito++;
-		if (op == 'n'){
-			inocente++; } }
-			
+	scanf("%c", &op);
+	if (op=='s')
+	{
+	suspeito++;
+	if (op == 'n')
+	{
+	inocente++; 
+	} 
+	}
+	fflush(stdin);
 	printf("Mora perto da vitima? ");
-	cin >> op;
-	if (op=='s'){
-		suspeito++;
-		if (op == 'n'){
-			inocente++; } }
-			
+	scanf("%c", &op);
+	if (op=='s')
+	{
+	suspeito++;
+	if (op == 'n')
+	{
+	inocente++; 
+	} 
+	}
+	fflush(stdin);	
 	printf("Devia a vitima? ");
-	cin >> op;
-	if (op=='s'){
-		suspeito++;
-		if (op == 'n'){
-			inocente++; } }
-			
+	scanf("%c", &op);
+	if (op=='s')
+	{
+	suspeito++;
+	if (op == 'n')
+	{
+	inocente++; 
+	} 
+	}
+	fflush(stdin);
 	printf("Ja trabalhou com a vitima? ");
-	cin >> op;
-	if (op=='s'){
-		suspeito++;
-		if (op == 'n'){
-			inocente++; } }
-			
-	if (suspeito==2){
-		printf("\nSuspeita"); }
-		else if (suspeito > 2 && suspeito <5){
-			printf("\nCumplice"); }
-			else if (suspeito==5){
-				printf("\nAssasino"); }
-				else {
-					printf("\nInocente"); }
-					
-	return 0; }
+	scanf("%c", &op);
+	if (op=='s')
+	{
+	suspeito++;
+	if (op == 'n')
+	{
+	inocente++; 
+	} 
+	}	
+	if (suspeito==2)
+	{
+	printf("\nSuspeita"); 
+	}
+	else if (suspeito > 2 && suspeito <5)
+	{
+	printf("\nCumplice"); 
+	}
+	else if (suspeito==5)
+	{
+	printf("\nAssasino"); 
+	}
+	else 
+	{
+	printf("\nInocente"); 
+	}		
+	return 0; 
+}
