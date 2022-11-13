5195233f24f88ebd8b5ab5113052e79bc527a042
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-11-13 16:49:14 -0300

    Update questao1.c

--- questao1.c
+++ questao1.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
-int main (){
+
+int main ()
+{
 	int num;
+	
 	printf("Digite um numero inteiro: ");
 	scanf("%d", &num);
-	if (num==1){
-		printf("\nDomingo"); }
-		else if (num==2){
-			printf("\nSegunda"); }
-			else if (num==3){
-				printf("\nTerca"); }
-				else if (num==4){
-					printf("\nQuarta"); }
-					else if (num==5){
-						printf("\nQuinta"); }
-						else if(num==6){
-							printf("\nSexta"); }
-							else if (num==7){
-								printf("\nSabado"); }
-								else {
-									printf("\nNumero invalido"); }
-	return 0; }
+	
+	if (num==1)
+	{
+	printf("\nDomingo"); 
+	}
+	else if (num==2)
+	{
+	printf("\nSegunda"); 
+	}
+	else if (num==3)
+	{
+	printf("\nTerca"); 
+	}
+	else if (num==4)
+	{
+	printf("\nQuarta"); 
+	}
+	else if (num==5)
+	{
+	printf("\nQuinta"); 
+	}
+	else if(num==6)
+	{
+	printf("\nSexta"); 
+	}
+	else if (num==7)
+	{
+	printf("\nSabado"); 
+	}
+	else 
+	{
+	printf("\nNumero invalido"); 
+	}
+	return 0; 
+}
