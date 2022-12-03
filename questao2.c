95e1f4c0860dc248ae1db4bad908edf529b34ba0
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-12-03 18:16:27 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -7,72 +7,61 @@ int main()
 	
 	printf("Telefonou para a vitima? ");
 	scanf("%c", &op);
-	if (op=='s')
-	{
+	
+	if (op=='s'){
 	suspeito++; 
-	if (op=='n')
-	{
+	if (op=='n'){
 	inocente++; 
-	} 
+		} 
 	}
+	
 	fflush(stdin);
 	printf("Esteve no local do crime? ");
 	scanf("%c", &op);
-	if (op=='s')
-	{
+	
+	if (op=='s'){
 	suspeito++;
-	if (op == 'n')
-	{
+	if (op == 'n'){
 	inocente++; 
-	} 
+		} 
 	}
 	fflush(stdin);
 	printf("Mora perto da vitima? ");
 	scanf("%c", &op);
-	if (op=='s')
-	{
+	if (op=='s'){
 	suspeito++;
-	if (op == 'n')
-	{
+	if (op == 'n'){
 	inocente++; 
-	} 
+		} 
 	}
 	fflush(stdin);	
 	printf("Devia a vitima? ");
 	scanf("%c", &op);
-	if (op=='s')
-	{
+	if (op=='s'){
 	suspeito++;
-	if (op == 'n')
-	{
+	if (op == 'n'){
 	inocente++; 
-	} 
+		} 
 	}
 	fflush(stdin);
 	printf("Ja trabalhou com a vitima? ");
 	scanf("%c", &op);
-	if (op=='s')
-	{
+	if (op=='s'){
 	suspeito++;
-	if (op == 'n')
-	{
+	if (op == 'n'){
 	inocente++; 
-	} 
+		} 
 	}	
-	if (suspeito==2)
-	{
+	if (suspeito==2){
 	printf("\nSuspeita"); 
 	}
-	else if (suspeito > 2 && suspeito <5)
-	{
+	else if (suspeito > 2 && suspeito <5){
 	printf("\nCumplice"); 
 	}
-	else if (suspeito==5)
-	{
+	else if (suspeito==5){
 	printf("\nAssasino"); 
 	}
-	else 
-	{
+	else {
 	printf("\nInocente"); 
 	}		
 	return 0; 
